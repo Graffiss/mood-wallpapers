1c297261f0e1cbd45b1b5639e6fff5ea33d957b5
Graffiss <graffis@gmail.com>
2020-05-18 14:56:56 +0200

    layout zdjec

--- src/components/organisms/Navbar/Navbar.js
+++ src/components/organisms/Navbar/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faImages, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
-    <nav className="navbar is-warning" role="navigation" aria-label="main navigation">
+    <nav className="navbar is-warning is-spaced" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" alt="logo" />
-        </a>
+        <NavLink className="navbar-item" exact to="/">
+          <h5 className="title is-5">Mood Wallpapers</h5>
+        </NavLink>
 
         <button
           type="button"
@@ -24,10 +26,16 @@ const Navbar = () => {
       <div id="navbarBasicExample" className="navbar-menu">
         <div className="navbar-start">
           <NavLink className="navbar-item" exact to="/">
+            <span className="icon">
+              <FontAwesomeIcon icon={faImages} />
+            </span>
             Gallery
           </NavLink>
 
           <NavLink className="navbar-item" to="/favourites">
+            <span className="icon">
+              <FontAwesomeIcon icon={faHeart} />
+            </span>
             Favourites
           </NavLink>
         </div>
--- src/views/GalleryView.js
+++ src/views/GalleryView.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import cx from 'classnames';
+import styles from './GalleryView.module.scss';
+import yellowCity from '../assets/images/yellow-city.jpg';
 
 const GalleryView = () => {
   return (
-    <div>
-      <h2>Galeria tapet:</h2>
+    <div className={cx(styles.view, 'tile is-ancestor')}>
+      <div className="tile is-vertical">
+        <div className="tile">
+          <div className="tile is-parent is-vertical">
+            <div className="tile is-child box">
+              <img src={yellowCity} alt="" className={styles.image} />
+            </div>
+            <div className="tile is-child box">
+              <img src={yellowCity} alt="" className={styles.image} />
+            </div>
+          </div>
+          <div className="tile is-parent">
+            <div className="tile is-child box">
+              <img src={yellowCity} alt="" className={styles.image} />
+            </div>
+          </div>
+        </div>
+        <div className="tile is-parent">
+          <div className="tile is-child box">
+            <img src={yellowCity} alt="" className={styles.image} />
+          </div>
+        </div>
+      </div>
+      <div className="tile is-parent">
+        <div className="tile is-child box">
+          <img src={yellowCity} alt="" className={styles.image} />
+        </div>
+      </div>
     </div>
   );
 };
