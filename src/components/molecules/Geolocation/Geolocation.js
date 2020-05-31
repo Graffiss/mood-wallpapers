1ee85044a6188369b8f580f830631aee28b5c2fd
Graffiss <graffis@gmail.com>
2020-05-31 17:18:26 +0200

    Revert "usuniecie wrazliwych plikow"
    This reverts commit cd8e88afb3927e90847d6dfec12e83d0689e189f.
    
    "revert"

---
+++ src/components/molecules/Geolocation/Geolocation.js
@@ -0,0 +1,48 @@
+/* mport React, { useEffect, useState } from 'react';
+import { fetchGeolocation } from '../../../api/geolocationApi';
+import { fetchWeather } from '../../../api/darkSkyApi';
+
+const Geolocation = () => {
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+  const [city, setCity] = useState([]);
+  const [weather, setWeather] = useState([]);
+
+  const onChange = ({ coords }) => {
+    setPosition({
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+    });
+  };
+  const onError = (error) => {
+    setError(error.message);
+  };
+
+  useEffect(() => {
+    const geo = window.navigator.geolocation;
+    if (!geo) {
+      setError('Geolocation is not supported');
+      return;
+    }
+    const watcher = geo.getCurrentPosition(onChange, onError);
+    return () => geo.clearWatch(watcher);
+  }, []);
+
+  useEffect(() => {
+    fetchGeolocation(setCity);
+  }, []);
+
+  useEffect(() => {
+    fetchWeather(setWeather);
+  }, []);
+
+  return (
+    <div>
+      <p>
+        We have a {weather} weather in {city}
+      </p>
+    </div>
+  );
+};
+
+export default Geolocation; */
